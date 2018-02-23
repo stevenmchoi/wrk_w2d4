0a959824c6f7496c40f1ed140b7cc82149f2e8a4
Steven Choi <steven.m.choi@gmail.com>
2018-02-22 16:36:54 -0800

    Implement #largest_contiguous_subsum and rspec tests

--- execution_time_differences/lib/etd.rb
+++ execution_time_differences/lib/etd.rb
@@ -3,16 +3,54 @@ def my_min_slow(array)
     return el if array.all? { |other_el| el <= other_el }
   end
 end
-# The time complexity for the function is:
-# - O(n**2)
+# The time and space complexity for the function are:
+# - O(n**2) runtime and O(1) space
 
 def my_min_fast(array)
   return nil if array.empty?
   min = array[0]
-  array[1..-1].each do |el|
-    min = el if el < min
+
+  index = 1
+  until index == array.length
+    min = array[index] if array[index] < min
+    index += 1
   end
+
   min
 end
-# The time complexity for the function is:
-# - O(n)
+# The time and space complexity for the function are:
+# - O(n) runtime and O(1) space
+
+def largest_contiguous_subsum_slow(array)
+  subarrays = []
+  array.each_index do |left_index|
+    (left_index...array.length).each do |right_index|
+      subarrays.push(array[left_index..right_index])
+    end
+  end
+  subarrays.map! { |arr| arr.reduce(:+) }.max
+end
+# The time and space complexity for the function are:
+# - O(n**3) runtime and O(n**3) space
+
+def largest_contiguous_subsum_fast(array)
+  return nil if array.empty?
+  max_subsum = array[0]
+  current_sum = array[0]
+
+  index = 1
+  until index == array.length
+    if array[index] > current_sum + array[index]
+      current_sum = array[index]
+    else
+      current_sum += array[index]
+    end
+
+    max_subsum = current_sum if current_sum > max_subsum
+    index += 1
+  end
+
+  max_subsum
+end
+# The time and space complexity for the function are:
+# - O(n) runtime and O(1) space
--- execution_time_differences/spec/etd_spec.rb
+++ execution_time_differences/spec/etd_spec.rb
@@ -2,17 +2,45 @@ require "rspec"
 require "etd"
 
 RSpec.describe "etd" do
-  let(:list) { [0, 3, 5, 4, -5, 10, 1, 90] }
+  describe "my_min" do
 
-  describe "#my_min_slow" do
-    it "finds the min element in an array" do
-      expect(my_min_slow(list)).to be(-5)
+    let(:list) { [0, 3, 5, 4, -5, 10, 1, 90] }
+
+    describe "#my_min_slow" do
+      it "finds the min element in an array" do
+        expect(my_min_slow(list)).to be(-5)
+      end
     end
+
+    describe "#my_min_fast" do
+      it "finds the min element in an array" do
+        expect(my_min_fast(list)).to be(-5)
+      end
+    end
+
   end
 
-  describe "#my_min_fast" do
-    it "finds the min element in an array" do
-      expect(my_min_fast(list)).to be(-5)
+  describe "largest_contiguous_subsum" do
+
+    let(:list1) { [5, 3, -7] }
+    let(:list2) { [2, 3, -6, 7, -6, 7] }
+    let(:list3) { [-5, -1, -3] }
+
+    describe "#largest_contiguous_subsum_slow" do
+      it "finds the largest contiguous subsum" do
+        expect(largest_contiguous_subsum_slow(list1)).to be(8)
+        expect(largest_contiguous_subsum_slow(list2)).to be(8)
+        expect(largest_contiguous_subsum_slow(list3)).to be(-1)
+      end
     end
+
+    describe "#largest_contiguous_subsum_fast" do
+      it "finds the largest contiguous subsum" do
+        expect(largest_contiguous_subsum_fast(list1)).to be(8)
+        expect(largest_contiguous_subsum_fast(list2)).to be(8)
+        expect(largest_contiguous_subsum_fast(list3)).to be(-1)
+      end
+    end
+
   end
 end
